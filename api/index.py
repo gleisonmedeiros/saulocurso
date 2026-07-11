b6e724a6e84786368324b0834d87f15f84d5c78f
gleisongpm@gmail.com <@06079292Gpm>
2026-07-11 02:20:04 -0300

    VERCEL

---
+++ api/index.py
@@ -0,0 +1,8 @@
+import os
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from config.wsgi import application as app
