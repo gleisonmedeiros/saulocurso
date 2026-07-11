c9220e331c2e2a545dacbcf8085fe8347126d3c0
gleisongpm@gmail.com <@06079292Gpm>
2026-07-11 02:34:27 -0300

    Adiciona estaticos do Django admin (CSS/JS) pro Vercel servir

--- config/settings.py
+++ config/settings.py
@@ -101,6 +101,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / '_staticfiles_build'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
